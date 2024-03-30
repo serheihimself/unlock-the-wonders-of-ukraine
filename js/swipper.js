b0071a3333e2f422b91115f6b5c29f541fa112b9
serheihimself <sergm080@gmail.com>
2024-03-31 00:38:17 +0200

    use swipper commit

---
+++ js/swipper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 2,
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
